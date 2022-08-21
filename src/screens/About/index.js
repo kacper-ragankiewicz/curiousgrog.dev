9762a21382876d1f35632cf2a906380be204454c
kacper-ragankiewicz <kacper-ragankiewicz@wp.pl>
2022-08-21 20:45:50 +0200

    Adding elements to about

--- src/screens/About/index.js
+++ src/screens/About/index.js
@@ -21,29 +21,54 @@ const About = ({ className }) => {
             <div className={styles.line}>
                 <Box
                     title="Skills"
-                    classTitle="title-blue"
-                />
+                >
+                    <div className={styles.liner}>
+                        <div className={styles.element}>JavaScript</div>
+                        <div className={styles.element}>TypeScript</div>
+                    </div>
+                    <div className={styles.liner}>
+                        <div className={styles.element}>React</div>
+                        <div className={styles.element}>CSS</div>
+                    </div>
+                    <div className={styles.liner}>
+                        <div className={styles.element}>NodeJS</div>
+                        <div className={styles.element}>SQL</div>
+                    </div>
+                </Box>
                 <Box
                     title="About"
-                    classTitle="title-red"
-                />
+                >
+                    <span className={styles.span}>Hi, My name is Kacper</span>
+
+                    <p className={styles.paragraf}>I'm Fullstack Developer and Cyber Security researcher. For dayly bases I spend most of my time on developing my projects and implementing new ideas.</p>
+                </Box>
             </div>
             <div className={styles.line}>
                 <Box
                     title="Known Technologies"
-                    classTitle="title-green"
-                />
+                    className={styles.know}
+                >
+                <div className={styles.elementwrapper}>
+                        <div className={styles.objectone}>React</div>
+                        <div className={styles.objecttwo}>Sass</div>
+                        <div className={styles.objectthree}>NodeJS</div>
+                        <div className={styles.objectfour}>SQL</div>
+                        <div className={styles.objectfive}>JavaScript</div>
+                        <div className={styles.objectsix}>CSS</div>
+                        <div className={styles.objectseven}>HTML</div>
+                        <div className={styles.objecteight}>PostgreSQL</div>
+                        <div className={styles.objectnine}>Python</div>
+                        <div className={styles.objectten}>OWASP10</div>
+                        <div className={styles.objecteleven}>Linux</div>
+                        <div className={styles.objecttwelve}>C++</div>
+                </div>
+
+                </Box>
                 <Box
-                    title="Known Technologies"
-                    classTitle="title-yellow"
+                    title="Writing code since:"
                 />
             </div>
             <div className={styles.wrap}>
-                <Box
-                    title="Career"
-                    classTitle="title-purple"
-                    className={styles.career}
-                />
              </div>
         </div>
 
