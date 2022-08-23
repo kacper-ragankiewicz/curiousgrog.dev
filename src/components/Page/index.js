dcadd7cca34e3a6cb118890bbcbfc79ef1c91e83
kacper-ragankiewicz <kacper-ragankiewicz@wp.pl>
2022-08-23 03:34:14 +0200

    Adding footer

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -6,17 +6,18 @@ import styles from "./Page.module.sass";
 // components
 import Header from "../Header";
 
-const Page = ({wide, children, title, className }) => {
-    const [visible, setVisible] = React.useState(true)
+const Page = ({ wide, children, title, className }) => {
+  const [visible, setVisible] = React.useState(true);
 
-    if (visible === true)
-        setTimeout(() => {
-            setVisible(false)
-        }, 1000);
+  if (visible === true)
+    setTimeout(() => {
+      setVisible(false);
+    }, 1000);
 
-    return (
-        <>
-            <div className={cn(styles.loader, className, { [styles.loading]: visible })}>
+  return (
+    <>
+      {/* Loadding view */}
+      {/* <div className={cn(styles.loader, className, { [styles.loading]: visible })}>
                 <div className={styles.object}>
                     <svg
                         viewBox="0 0 24 24"
@@ -30,20 +31,23 @@ const Page = ({wide, children, title, className }) => {
 
                     </svg>
                 </div>
-            </div>
-            <div className={styles.page}>
-                <Header/>
-                <div
-                    className={cn(styles.container, {
-                        [styles.wide]: wide,
-                    })}
-                >
-                    {title && <div className={cn("h3", styles.title)}>{title}</div>}
-                    {children}
-                </div>
-            </div>
-        </>
-    )
+            </div> */}
+      <div className={styles.page}>
+        <Header className={styles.header} />
+        <div
+          className={cn(styles.container, {
+            [styles.wide]: wide,
+          })}
+        >
+          {title && <div className={cn("h3", styles.title)}>{title}</div>}
+          {children}
+        </div>
+      </div>
+      <footer className={styles.footer}>
+        <div className={styles.body}></div>
+      </footer>
+    </>
+  );
 };
 
 export default withRouter(Page);
