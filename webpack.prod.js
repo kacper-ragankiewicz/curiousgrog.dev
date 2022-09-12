7358e18c57990c8bd3247711257845b80e8ee690
kacper-ragankiewicz <kacper-ragankiewicz@wp.pl>
2022-09-12 11:57:57 +0200

    Adding extractor for styles

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,6 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = merge(common, {
   mode: 'production',
-   module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: ExtractTextPlugin.extract({
-          fallback: "style-loader",
-          use: "css-loader"
-        })
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin("styles.css"),
-  ]
 });
